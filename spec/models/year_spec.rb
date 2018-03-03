c46b30fb788d1a2f2022cf97fd3f237ae8218624
Kelly Munro <kellymdev@gmail.com>
2018-03-03 18:16:58 +1300

    Refactor invalid year number specs into shared examples

--- spec/models/year_spec.rb
+++ spec/models/year_spec.rb
@@ -13,22 +13,23 @@ RSpec.describe Year, type: :model do
     end
 
     context 'with an invalid year number' do
-      context 'when the year number is too short' do
-        let(:year_number) { 201 }
-
+      shared_examples 'invalid year number' do
         it 'is invalid' do
           expect(year).not_to be_valid
           expect(year.errors.full_messages).to include('Year number must be between 1000 and 9999')
         end
       end
 
-      context 'when the year number is too long' do
-        let(:year_number) { 12345 }
+      context 'when the year number is too short' do
+        let(:year_number) { 201 }
 
-        it 'is invalid' do
-          expect(year).not_to be_valid
-          expect(year.errors.full_messages).to include('Year number must be between 1000 and 9999')
-        end
+        it_behaves_like 'invalid year number'
+      end
+
+      context 'when the year number is too long' do
+        let(:year_number) { 12_345 }
+
+        it_behaves_like 'invalid year number'
       end
     end
   end
