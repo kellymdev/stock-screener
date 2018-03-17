0cefa56de4d95f06e40b7fbadb04bcc3d649fbb5
Kelly Munro <kellymdev@gmail.com>
2018-03-17 16:40:43 +1300

    Add seed data for Year

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
 # Stock Exchanges
-asx = StockExchange.create!(name: 'ASX')
-nzx = StockExchange.create!(name: 'NZX')
-nyse = StockExchange.create!(name: 'NYSE')
+# asx = StockExchange.create!(name: 'ASX')
+# nzx = StockExchange.create!(name: 'NZX')
+# nyse = StockExchange.create!(name: 'NYSE')
+
+# Years
+years = 2007..2017
+
+years.each do |year|
+  Year.create!(year_number: year)
+end
