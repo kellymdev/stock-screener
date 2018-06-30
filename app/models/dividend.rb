03536918a11e4d4602ac4e01374d7ae1b11a6c13
Kelly Munro <kellymdev@gmail.com>
2018-06-30 17:22:12 +1200

    Add for_year scope to dividend

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -5,4 +5,6 @@ class Dividend < ApplicationRecord
   belongs_to :year
 
   validates :value, presence: true, numericality: true
+
+  scope :for_year, ->(year) { where(year: Year.find_by(year_number: year)) }
 end
