72a30bfa39d58c2a25dfe78a0908fea27e269d4e
Kelly Munro <kellymdev@gmail.com>
2018-08-04 14:06:03 +1200

    Display earnings, dividends and share prices sorted by year on the Stock Details screen

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -6,5 +6,6 @@ class Dividend < ApplicationRecord
 
   validates :value, presence: true, numericality: true
 
+  scope :by_year, -> { joins(:year).order("years.year_number") }
   scope :for_year, ->(year) { where(year: Year.find_by(year_number: year)) }
 end
